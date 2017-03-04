6740453f4aacc1b334ff59e95dac08d6b1ed22dd
zhaozhiming <zhaozhiming003@gmail.com>
2017-03-04 19:58:04 +0800

    execrise 2.x

--- nature_code_02/mover.js
+++ nature_code_02/mover.js
@@ -1,62 +1,67 @@
 class Mover {
-  constructor() {
-    this.location = createVector(random(width / 2), random(height / 2));
-    this.velocity= createVector(random(-2, 2), random(-2, 2));
-    this.acceleration = p5.Vector.random2D();
-    this.acceleration.mult(map(noise(0.01), 0, 1, -0.01, 0.01));
-    this.topSpeed = 10;
+  constructor(x, y, mass) {
+    this.mass = mass;
+    this.location = createVector(x, y);
+    this.velocity= createVector(0, 0);
+    this.acceleration = createVector(0, 0);
   }
 
   update() {
     this.velocity.add(this.acceleration);
     this.location.add(this.velocity);
-    this.acceleration.mult(0);
   }
 
   display() {
     stroke(0);
     fill(175);
-    ellipse(this.location.x, this.location.y, 16, 16);
+    ellipse(this.location.x, this.location.y, this.mass * 16, this.mass * 16);
   }
 
   checkEdges() {
     if (this.location.x > width) {
-      this.location.x = 0;
-    } else if (this.location.x < 0) {
       this.location.x = width;
+      this.velocity.x *= -1;
+    } else if (this.location.x < 0) {
+      this.location.x = 0;
+      this.velocity.x *= -1;
     }
     if (this.location.y > height) {
-      this.location.y = 0;
-    } else if (this.location.y < 0) {
       this.location.y = height;
+      this.velocity.y *= -1;
+    } else if (this.location.y < 0) {
+      this.location.y = 0;
+      this.velocity.y *= -1;
     }
   }
 
   applyForce(force) {
-    this.acceleration.add(force);
+    const f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
   }
 }
 
-let mover;
-let windPower = 0;
+let movers = [];
 function setup() {
   createCanvas(500, 500);
   smooth();
-  mover = new Mover();
+  for (let i = 0; i < 100; i++) {
+    movers.push(new Mover(0, 0, random(0.1, 5)));
+  }
 }
 
 function draw() {
   background(255);
-  const wind = createVector(map(noise(windPower), 0, 1, -0.01, 0.01), 0);
-  mover.applyForce(wind);
-  windPower += 0.01;
-  mover.update();
-  mover.checkEdges();
-  mover.display();
-  console.log({ mag: mover.velocity.mag() });
-}
 
-function mousePressed() {
-  const wind = createVector(0.5, 0);
-  mover.applyForce(wind);
+  for (let i = 0; i < movers.length; i++) {
+    const wind = createVector(0.001, 0);
+    const m = movers[i].mass;
+    const gravity = createVector(0, 0.1 * m);
+
+    movers[i].applyForce(wind);
+    movers[i].applyForce(gravity);
+
+    movers[i].update();
+    movers[i].display();
+    movers[i].checkEdges();
+  }
 }
